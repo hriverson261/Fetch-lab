b59f26961d978367df73abbd8a55835584b6e0dd
Henrrietta <hriverson261@gmail.com>
2026-04-10 10:49:35 -0400

    Initial project setup

--- sports-app/src/App.jsx
+++ sports-app/src/App.jsx
@@ -1,1 +1,51 @@
+//  "https://www.thesportsdb.com/free_sports_api" - 
+// this is the sports api I'm going to use for my app 
+// src/components/ApiData.jsx
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+const App = () => {
+
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+
+      const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+      const data = await response.json();
+
+      setData(data);
+      setLoading(false);  
+
+      useEffect(() => {
+        fetchData();
+      }, []);
+    };
+  }, []);
+
+  return (
+      <div className="app">
+      <h1>Data App</h1>
+
+       
+
+      {loading && <p>Loading Data...</p>}
+
+      {Data && !loading && (
+        <div className="data-card">
+
+        </div>
+      )}
+
+    </div>
+  );
+};
+
+export default App;
+
+
 
